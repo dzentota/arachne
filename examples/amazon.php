f8b0f3e025b4c5ed453ac6aaa757c2f679c89305
Alex Tatulchenkov <atatulchenkov@sugarcrm.com>
2018-11-05 18:03:20 +0300

    docker based env

--- examples/amazon.php
+++ examples/amazon.php
@@ -1,4 +1,5 @@
 <?php
+
 require 'vendor/autoload.php';
 
 use Arachne\Client\Events\ResponseReceived;
@@ -9,6 +10,7 @@ use Arachne\Identity\Identity;
 use Psr\Http\Message\ResponseInterface;
 use Arachne\Crawler\DomCrawler;
 use Arachne\ResultSet;
+use Psr\Log\LoggerInterface;
 use Respect\Validation\Validator as v;
 use Arachne\Identity\IdentitiesCollection;
 
@@ -19,7 +21,16 @@ $container = \Arachne\Service\Container::create(new \Arachne\Service\Proxy(
     {
         protected function getDbName()
         {
-            return 'kaina';
+            return 'amazon';
+        }
+
+        protected function createDelayHandler(LoggerInterface $logger)
+        {
+            return function ($retries) use ($logger) {
+                $delay = 1000 * (int)pow(2, $retries - 1);
+                $logger->debug("Sleeping $delay milliseconds before retry");
+                return $delay;
+            };
         }
 
         protected function getMongoDbClient(): \MongoDB\Client
@@ -57,6 +68,7 @@ class Product extends \Arachne\Item
     protected $title;
     protected $url;
     protected $mapped;
+    protected $price;
 
     protected $type = 'product';
 
@@ -64,19 +76,20 @@ class Product extends \Arachne\Item
     {
         return v::attribute('title', v::stringType())
             ->attribute('url', v::stringType())
-            ->attribute('mapped', v::arrayVal()->each(v::stringType()))
+            ->attribute('price', v::stringType())
+//            ->attribute('mapped', v::arrayVal()->each(v::stringType()))
             ;
 
     }
 
-    public function normalizeUrl()
-    {
-        $purl = new \Purl\Url($this->url);
-        $purl->query->remove('utm_source');
-        $purl->query->remove('utm_medium');
-        $purl->query->remove('utm_campaign');
-        $this->url = $purl->getUrl();
-    }
+//    public function normalizeUrl()
+//    {
+//        $purl = new \Purl\Url($this->url);
+//        $purl->query->remove('utm_source');
+//        $purl->query->remove('utm_medium');
+//        $purl->query->remove('utm_campaign');
+//        $this->url = $purl->getUrl();
+//    }
 }
 
 $container->get()->eventDispatcher()
@@ -90,7 +103,7 @@ $container->get()
     ->scraper()
     ->scrape([
         'frontier' => [
-            ['url' => 'https://www.kaina24.lt/top/', 'type' => 'top'],
+            ['url' => 'https://www.amazon.com/s/s/ref=sr_nr_p_n_availability_1?rh=n%3A2619533011%2Cn%3A%212619534011%2Cn%3A2975312011%2Cp_72%3A2661618011%2Cp_n_availability%3A2661601011&bbn=2975312011&ie=UTF8&qid=1541426639', 'type' => 'category'],
         ],
         'success:top' => function (ResponseInterface $response, ResultSet $resultSet) use ($container) {
             $content = (string)$response->getBody();
@@ -102,27 +115,29 @@ $container->get()
         'success:category' => function (ResponseInterface $response, ResultSet $resultSet) use ($container) {
             $content = (string)$response->getBody();
             $crawler = new DomCrawler($content);
-            $crawler->filter('.product-item .details-1 h2 a')->each(function (DomCrawler $crawler) use ($resultSet){
-                $resultSet->addHighPriorityResource('item', $crawler->attr('href'));
+            $crawler->filter('#atfResults .s-item-container')->each(function (DomCrawler $crawler) use ($resultSet){
+                $prime = $crawler->filter('.a-icon-prime')->attr('class');
+//                if (!empty($prime)) {
+                    $link = $crawler->filter('a.s-access-detail-page')->attr('href');
+                    $resultSet->addHighPriorityResource('item', $link);
+//                }
+            });
+            $crawler->filter('.pagnLink a')->each(function (DomCrawler $crawler) use ($resultSet) {
+                $link = 'https://www.amazon.com' . $crawler->attr('href');
+                $resultSet->addResource('category', $link);
             });
         },
 
         'success:item' => function (ResponseInterface $response, ResultSet $resultSet) {
             $content = (string)$response->getBody();
             $crawler = new DomCrawler($content);
-            $data['title'] = str_replace(' kaina', '', $crawler->filter('h1')->text());
+            $data['title'] =  trim($crawler->filter('#title')->text());
             $data['url'] = $resultSet->getResource()->getUrl();
-            $mapped = [];
-            $crawler->filter('.seller-item-table h3 a')->each(function(DomCrawler $crawler) use (&$mapped) {
-                $onclick = $crawler->attr('onclick');
-                preg_match("~'eventAction':\s*'(.*?)'~is", $onclick, $m);
-                if (!empty($m[1])) {
-                    $mapped[] = $m[1];
-                }
-            });
-            $data['mapped'] = $mapped;
+            var_dump($crawler->filter('#priceblock_ourprice')->text());
+            $data['price'] = trim($crawler->filter('#priceblock_ourprice')->text());
+
             $item = new Product($data);
-            $item->normalizeUrl();
+      //      $item->normalizeUrl();
             $resultSet->addItem($item);
 //            print_r($item);
 //            die();
